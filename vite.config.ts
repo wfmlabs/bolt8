1af5f381ff3b13239afbf56e1fd3428f52edee14
WFM Labs <116904117+wfmlabs@users.noreply.github.com>
2024-12-07 16:41:55 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bolt8/', // Add this line with your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
